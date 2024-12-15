92b68ebfdb969f6afc2d5f097c0ccb1f9d4c3bfb
Mars1836 <hauhpll123@gmail.com>
2024-12-15 16:27:24 +0700

    a

--- server/app/utils/index.py
+++ server/app/utils/index.py
@@ -6,7 +6,6 @@ def generate_random_number(length=6):
     return ''.join(random.choices('0123456789', k=length))
 def store_image(file,license_plate):
     file_name = file.filename
-    save_path = ('/static/images/',generate_random_number() + file_name )
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    save_path = os.path.join("/static/images/",generate_random_number() + file_name )
     file.save(save_path)
     return save_path
