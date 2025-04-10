1478021c329ee822e6ace12a36cde07a615b7a04
Mars1836 <hauhpll123@gmail.com>
2025-04-10 11:39:00 +0700

    a

--- server/app/utils/index.py
+++ server/app/utils/index.py
@@ -8,6 +8,8 @@ def store_image(file,license_plate):
     file_name = file.filename
     random_number = generate_random_number()
     new_file_name = random_number + file_name
-    save_path = os.path.join("app/static/images/",new_file_name )
+    save_path = os.path.join("app/static/images/",new_file_name)
+    # Create directory if it doesn't exist
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
     file.save(save_path)
     return "/images/"+new_file_name
